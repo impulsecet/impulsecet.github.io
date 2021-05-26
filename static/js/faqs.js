83675ffb8faebbae9e4b105615945240a520aa9d
Rahul T <rahulmanoj@cet.ac.in>
2021-05-26 20:41:05 +0530

    update

--- static/js/faqs.js
+++ static/js/faqs.js
@@ -1,3 +1,31 @@
+/* Guide To Write Faq's
+const faqs = [
+    {
+        q: "what is impulse?",
+        a: "IMPULSE will be a series of exciting events, each leading to another. <br> The primary goal is to provide upcoming engineers a peek into state-of-the-art research and industry practices, and to direct them towards it."
+    },
+    {
+        q: "when is impulse?",
+        a: "this is <b> Bold <b>"
+    },
+    {
+        q: "what is hackathon?",
+        a: "that is a big thing <br> This is going to be in the next line"
+    },
+]
+
+============ POINTS TO REMEMBER ===========
+- Don't forget commas (,) 
+- write the question and answer in a line
+
+============= TIPS =================
+- To make a line break you can add <br>
+- For bold <b> some bold content</b>
+
+*/
+
+
+
 const faqs = [
     {
         q: "what is impulse?",
