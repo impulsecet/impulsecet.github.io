b432d9b5c231408598b4bb3c368a567ada0967e0
Rahul T <rahulmanoj@cet.ac.in>
2021-05-26 20:53:59 +0530

    Update faqs.js

--- static/js/faqs.js
+++ static/js/faqs.js
@@ -29,6 +29,6 @@ const faqs = [
 const faqs = [
     {
         q: "what is impulse?",
-        a: "IMPULSE will be a series of exciting events, each leading to another. <br> The primary goal is to provide upcoming engineers a peek into state-of-the-art research and industry practices, and to direct them towards it."
+        a: "IMPULSE will be a series of exciting events, each leading to another. The primary goal is to provide upcoming engineers a peek into state-of-the-art research and industry practices, and to direct them towards it."
     },
 ]
