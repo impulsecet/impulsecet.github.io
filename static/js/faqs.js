f2450e4c9efdbca1294233dce7c32e7ec1b1d76c
Rahul T <rahulmanoj@cet.ac.in>
2021-05-27 11:16:24 +0530

    update

--- static/js/faqs.js
+++ static/js/faqs.js
@@ -2,21 +2,23 @@
 const faqs = [
     {
         q: "what is impulse?",
-        a: "IMPULSE will be a series of exciting events, each leading to another. <br> The primary goal is to provide upcoming engineers a peek into state-of-the-art research and industry practices, and to direct them towards it."
+        a: `<b> IMPULSE </b> will be a series of exciting events, <br>
+            each leading to another. <br> The primary goal is to provide upcoming engineers  <br>
+            a peek into state-of-the-art research and industry practices, and to direct them towards it.`
     },
     {
         q: "when is impulse?",
-        a: "this is <b> Bold <b>"
+        a: `this is <b> Bold <b>`
     },
     {
         q: "what is hackathon?",
-        a: "that is a big thing <br> This is going to be in the next line"
+        a: `that is a big thing <br>
+           This is going to be in the next line`
     },
 ]
 
 ============ POINTS TO REMEMBER ===========
 - Don't forget commas (,) 
-- write the question and answer in a line
 
 ============= TIPS =================
 - To make a line break you can add <br>
@@ -29,6 +31,7 @@ const faqs = [
 const faqs = [
     {
         q: "what is impulse?",
-        a: "IMPULSE will be a series of exciting events, each leading to another. <br> The primary goal is to provide upcoming engineers a peek into state-of-the-art research and industry practices, and to direct them towards it."
-    },
+        a: `<b>IMPULSE</b> will be a series of exciting events, each leading to another.<br> 
+            The primary goal is to provide upcoming engineers a peek into state-of-the-art research and industry practices, and to direct them towards it.`
+    },    
 ]
