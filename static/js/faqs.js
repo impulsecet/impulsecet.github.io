43495c7bbb12ac3119821935f906d1ebb310da3c
Rahul T <rahulmanoj@cet.ac.in>
2021-05-27 11:55:18 +0530

    new faqs

--- static/js/faqs.js
+++ static/js/faqs.js
@@ -34,8 +34,38 @@ const faqs = [
 
 const faqs = [
     {
-        q: "what is impulse?",
-        a: `<b>IMPULSE</b> will be a series of exciting events, each leading to another.<br> 
-            The primary goal is to provide upcoming engineers a peek into state-of-the-art research and industry practices, and to direct them towards it.`
+        q: "What is a hackathon?",
+        a: `A Hackathon is an event where people/participants work with their team to solve a particular problem statement or challenge.`
     },    
+    {
+        q: "How to approach the IMPULSE hackathon?",
+        a: `
+            IPULSE hackathon is a 3-day long hackathon, which spans from 28th May 12 AM to 30th May 11.59 PM. <br>
+            Your team will be given a problem statement, for which you'll get these three days to solve. <br>
+            Solutions can be in any one of these concepts (only). <br>
+            <ul> 
+                <li>Machine Learning</li>
+                <li> CV </li>
+                <li> DSP </li>
+            </ul>
+
+        `
+    },
+    {
+        q: "How to register for the hackathon?",
+        a: `Only one member from each team is to register. 
+            [Later this member will be added to a WhatsApp group where directions will be given, which he/she is to inform the rest of the group.] <br>
+            Registrations will be done through google forms which will be handed out soon. <br> <br>
+
+            Registration of teams with (only) the participants of the IMPULSE mentoring programme as its members, will be separate from teams which also includes non-participants as its members. <br>
+            [i.e., separate google forms will be given to both the groups] <br> <br>
+
+            <b>Note:</b>  Though the registrations for both the groups are separate, the tasks and the judgement will be overall, which is common to both he groups. But participants who were already a part of the IMPULSE mentoring programme may get their own benefits.
+
+        `
+    },
+    {
+        q: "What are the criteria to get the certificate of participation in the hackathon?",
+        a: `For you to earn a certificate, it is a must for your team to submit a solution to the problem statement.`
+    },
 ]
