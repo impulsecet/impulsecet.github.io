446d80af6b6b28de7b32600044266147335c727e
rahultmanoj <84894160+rahultmanoj@users.noreply.github.com>
2021-05-27 11:14:07 +0530

    Update faqs.js

--- static/js/faqs.js
+++ static/js/faqs.js
@@ -13,9 +13,15 @@ const faqs = [
         a: "that is a big thing <br> This is going to be in the next line"
     },
     {
-        q: "when is hackathon?",
+        q: "what is hackathon?",
         a: "that is a big thing <br> This is going to be in the next line"
     },
+    {
+        q: "what is hackathon?",
+        a: "that is a big thing <br> This is going to be in the next line"
+    },
+    
+    
 ]
 
 ============ POINTS TO REMEMBER ===========
